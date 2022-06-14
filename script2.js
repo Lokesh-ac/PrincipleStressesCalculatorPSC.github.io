4defaf7e057ce5879f80f02703cc7b0c2646c7e7
Lokesh AC <102301005+Lokesh-ac@users.noreply.github.com>
2022-06-14 18:59:56 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,20 @@
+
+/*
+$(document).ready(function(){
+  $("#Cal").fadeOut("fast");
+  $("#res").fadeOut("fast");
+});
+*/
+$(function(){
+  $("#Cal").hide("fast");
+  $("#res").hide("fast");
+  $("#Cal").slideDown(2000);
+  $("#calculate").click(function(){
+    $("#res").show(2000);
+  });
+
+  $("#refresh").click(function(){
+    $("#res").hide("fast");
+  });
+
+});
